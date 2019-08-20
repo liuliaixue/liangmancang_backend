6cbbabfac1a6b444238cf89f11fdb879aae4910b
liuliaixue <liuliaixue@qq.com>
2019-08-20 19:04:52 +0800

    ts

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,14 @@ const config = require('./config/config');
 const app = require('./config/express');
 require('./config/mongoose');
 
+import config from './config/config'
+
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
-  app.listen(config.port, () => {
-    console.info(`server started on port ${config.port} (${config.env})`);
-  });
+    app.listen(config.port, () => {
+        console.info(`server started on port ${config.port} (${config.env})`);
+    });
 }
 
 module.exports = app;
