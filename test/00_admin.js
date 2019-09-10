f91448114e9efd65694fb7170efef2dca9925ced
liuliaixue <liuliaixue@qq.com>
2019-09-11 07:01:14 +0800

    refine test

--- test/00_admin.js
+++ test/00_admin.js
@@ -3,30 +3,38 @@ const assert = require('assert');
 const client = require('./_client')();
 const { execSync } = require('child_process');
 
+const User = require('../dist/models/user.model').default;
+const connectMongo = require('../dist/config/mongoose');
 
 describe('Admin', function() {
   it('create admin user', async () => {
-    const {admin }= config
-    const res = await client.post('/api/auth/register', admin)
-    assert(res.data.user.username === admin.username)
-    const adminInfo = res.data.user
-    const script = `"
-      db = db.getSiblingDB('lmc');
+    const { admin } = config;
+    const res = await client.post('/api/auth/register', admin);
+    assert(res.data.user.username === admin.username);
+    const adminInfo = res.data.user;
+    // const script = `"
+    //   db = db.getSiblingDB('lmc');
 
-      db.getCollection('users').update({_id:ObjectId('${adminInfo._id}')},{
-        \\$set:{
-            'roles' : ['root'],
-          }
-      });
-    "`;
-    // update user's role 
-    execSync(`mongo --eval ${script.replace(/\n/gi, ' ')}`, {});
+    //   db.getCollection('users').update({_id:ObjectId('${adminInfo._id}')},{
+    //     \\$set:{
+    //         'roles' : ['root'],
+    //       }
+    //   });
+    // "`;
+    // execSync(`mongo --eval ${script.replace(/\n/gi, ' ')}`, {});
+
+    const r = await User.findByIdAndUpdate(adminInfo._id, {
+      $set: { roles: ['root'] }
+    }).catch(e => {
+      console.log(e);
+    });
+    console.log(r);
   });
 
   it('admin login', async () => {
     const res = await client.post('/api/auth/login', config.admin);
     assert(res.data.user.username === config.admin.username);
-    config.adminInfo = res.data.user
-    config.adminToken = res.data.token
+    config.adminInfo = res.data.user;
+    config.adminToken = res.data.token;
   });
 });
